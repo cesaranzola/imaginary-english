011dee1c09326e37ce22599bdc0e9e054c275640
Andres Rueda <camilo20021409@gmail.com>
2023-06-24 18:05:17 -0500

    332

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 # imaginaryEnglishTest
  
+=======
+# imaginary-english
+Java game using FX framework
+>>>>>>> parent of 5ab80a1 (inicio)
---
+++ src/Controller/PartidaLvL1Controller.java
@@ -0,0 +1,25 @@
+package Controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Andres Rueda
+ */
+public class PartidaLvL1Controller implements Initializable {
+     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        
+    }    
+    
+}
